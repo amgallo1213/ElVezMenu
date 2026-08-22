a689821902157b8d41ba079eac92bba40f46e305
Andrea <90292543+amgallo1213@users.noreply.github.com>
2026-08-21 23:08:59 -0400

    css update

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <div className="e-h1">
-        <h1 >404</h1>
-        <h2>Page Not Found</h2>
+    <div >
+        <h1 className="e-h1">404</h1>
+        <h2 className="e-h1">Page Not Found</h2>
     </div>
   )
 }
